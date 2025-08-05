0e51461143c4369d1495260aaea4db96898205d5
Paris Koloveas <pkoloveas@athenarc.gr>
2025-08-05 13:29:46 +0300

    Update next.config.ts

--- InsightGUIDE/frontend-web-ui/next.config.ts
+++ InsightGUIDE/frontend-web-ui/next.config.ts
@@ -1,6 +1,8 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
+  /* config options here */
+  future: { webpack5: true },
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -16,6 +18,11 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
     ],
+  },
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.resolve.alias.canvas = false;
+    config.resolve.alias.encoding = false;
+    return config;
   },
 };
 
